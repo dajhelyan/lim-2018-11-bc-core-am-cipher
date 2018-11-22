77564b5865a5056aa0dbbf06a6fb685666c8d6af
dajhelyan <assiaxramx10@gmail.com>
2018-11-22 13:17:30 -0500

    avances en estructura y js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,37 @@
 window.cipher = {
   // ... 
-};
+  encode: function(offset, string) {
+    console.log(offset, string)
+
+    let msg = '';
+    for (let i = 0; i < string.length; i++) {
+      // const letrita = string[index];
+      const codigo = (string[i].charCodeAt(0) - 65 + offset) % 26 + 65
+      const letra_cif = String.fromCharCode(codigo)
+      msg = msg + letra_cif;
+      // console.log(letra_cif);
+    }
+    return msg;
+    // msg = ''
+    // nombre = 'laika'
+    // for(var i = 0; i < nombre.length; i++){
+    //   msg = msg + nombre[i];
+    // }
+    // console.log(msg)
+  },
+
+  decode: function(offset, string) {
+    let msg_descifrar = '';
+    for(let i = 0; i < String.length; i++){
+      const codigo = (string[index].charCodeAt(0) - 65 - offset) % 26 + 65
+      alert("");
+    }
+    console.log(string)
+  }
+
+}
+
+//offset = 2;
+//string = 'LAILAZ';
+
+//console.log(cipher.encode(offset, string));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+const button_cifrado = document.getElementById('cifrar');
+const input = document.getElementById('mensaje');
+const resultado = document.getElementById('resultado');
 
+const click_button_cifrado = function(){
+  console.log('hiciste click en cifrar');
+  const offset = parseInt(document.getElementById('offset').value);
+  const msg = input.value;
+
+ const mensaje_cifrado = cipher.encode(offset, msg);
+ //mensaje_descrifado = cipher.encode(offset, msg_descrifado)
+ resultado.innerHTML = mensaje_cifrado;
+}
+
+button_cifrado.addEventListener('click', click_button_cifrado)
