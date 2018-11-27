9d6d737940e26daf097fac73c7890846f6bf6642
dajhelyan <assiaxramx10@gmail.com>
2018-11-27 13:03:44 -0500

    arreglar display y div

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ const button_cifrado = document.getElementById('cifrar');
 const button_decifrado = document.getElementById('decifrar')
 const input = document.getElementById('mensaje');
 const resultado = document.getElementById('resultado');
+const button_comenzar = document.getElementById('comenzar')
+const cifrado = document.getElementById('cifrado')
+const bienvenida = docuement.getElementById('bienvenida')
+
+const click_button_comenzar = function(){
+  cifrado.classList.add("mostrar");
+  cifrado.classList.remove("ocultar");
+  //bienvenida.classList.add("ocultar");
+  //bienvenida.classList.remove("mostrar");
+}
+
 
 const click_button_cifrado = function(){
   console.log('hiciste click en cifrar');
@@ -24,3 +35,4 @@ const click_button_decifrado = function(){
 
 button_cifrado.addEventListener('click', click_button_cifrado);
 button_decifrado.addEventListener('click', click_button_decifrado);
+button_comenzar.addEventListener('click', click_button_comenzar);
