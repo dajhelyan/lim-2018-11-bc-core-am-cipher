81529fa74846059dc428099484fc62537232a8e7
dajhelyan <assiaxramx10@gmail.com>
2018-12-06 13:12:05 -0500

    corre espacios en el cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -3,8 +3,13 @@ window.cipher = {
    encode:function (offset, string){
     let msg = '';    
     for (let i = 0; i < string.length; i++) {
-      if (string.charCodeAt(i) >= 65 || string.charCodeAt(i) <= 90) {
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
         const cifrar = (string[i].charCodeAt(0) - 65 + offset) % 26 + 65;
+        let letra_cif = String.fromCharCode(cifrar);
+        msg = msg + letra_cif;
+      }
+      else if(string.charCodeAt(i) === 32){
+        const cifrar = 32;
         let letra_cif = String.fromCharCode(cifrar);
         msg = msg + letra_cif;
       }
